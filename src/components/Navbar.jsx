1eebdd5ae71462619b4c2d11ec3b9d05b91d2a27
! YouPLe9999 <105734928+YouPLe9999@users.noreply.github.com>
2023-12-26 18:09:17 +0900

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            YouPLe &nbsp;{" "}
+            이연우 &nbsp;{" "}
             <span className="sm:block hidden">| 백엔드 개발자</span>
           </p>
         </Link>
