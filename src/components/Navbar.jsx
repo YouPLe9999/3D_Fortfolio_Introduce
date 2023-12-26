d6ae720377f37d99c501df3510cee242f1532474
! YouPLe9999 <105734928+YouPLe9999@users.noreply.github.com>
2023-12-26 18:05:58 +0900

    my name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Chobby &nbsp;{" "}
-            <span className="sm:block hidden">| 프론트엔드 개발자</span>
+            YouPLe &nbsp;{" "}
+            <span className="sm:block hidden">| 백엔드 개발자</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
